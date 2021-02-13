dd6dad9ee53166dd94029a4147a2ae73fa05664d
RD1K <48737891+RD1K@users.noreply.github.com>
2021-02-13 16:32:19 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ This is the main file of the program.
 
 import pretty_midi
 from os import system
-from random import randrange, randint
+from random import randrange, randint, uniform
 from playsound import playsound
+from pysndfx import AudioEffectsChain
+
 import generateScale
 import chordMaker
 
@@ -39,17 +41,8 @@ piano = pretty_midi.Instrument(program=piano_program)
 startTime = 0
 previousNoteNumber = 0
 
-# for note_name in scaleNotes:
-#     note_number = pretty_midi.note_name_to_number(note_name + "5")
-#     if note_number < previousNoteNumber:
-#         note_number += 12
-#     note = pretty_midi.Note(velocity=127, pitch = note_number, start=startTime, end=(beatLength))
-#     piano.notes.append(note)
-#     startTime += beatLength
-#     previousNoteNumber = note_number
-
 chordProgressionRoots = []
-for x in range(1,8):
+for x in range(1,9):
     chordProgressionRoots.append(scaleNotes[randrange(1,7)])
 
 chordDict = chordMaker.buildChord(chordProgressionRoots, scaleNotes)
@@ -57,11 +50,22 @@ chordDict = chordMaker.buildChord(chordProgressionRoots, scaleNotes)
 startTime = 0
 for key, value in chordDict.items():
     print(value)
-
+    for note_name in value:
+        nameWithoutOct = note_name
+        for x in range(4,6):
+            note_name = nameWithoutOct + str(x)
+            note_number = pretty_midi.note_name_to_number(note_name)
+            note = pretty_midi.note_name_to_number(note_name)
+            velocity = randint(105,127)
+            delay = round(uniform(0,0.05), 3) # generates a random amount of delay to make it sound more natural/realistic
+            note = pretty_midi.Note(velocity = velocity, pitch = note_number, start = (startTime + delay), end = startTime + (4*beatLength))
+            piano.notes.append(note)
+    startTime += 4*beatLength # each chord plays for 4 beats, which is equal to 1 bar since it's in 4/4
 
 pianoMelody.instruments.append(piano)
 pianoMelody.write("pianoMelody.mid")
 
 # synthesizes the MIDI using the soundfont
 system("fluidsynth -ni %s pianoMelody.mid -F output.wav -r 44100 2>/dev/null" % sfDir)
-playsound("output.wav")
+for x in range(1,3):
+    playsound("output.wav")
