c40435321f2efc77ef91c58ea10941d837921eab
RD1K <48737891+RD1K@users.noreply.github.com>
2021-02-13 14:46:29 -0500

    Add files via upload

---
+++ generateScale.py
@@ -0,0 +1,34 @@
+'''
+This file contains data such as all of the music notes, and the formulas for different
+scales. It also determines the notes in a scale based on the key and mode.
+'''
+
+notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+scales = ['Major', 'Minor']
+
+# formulas for major/natural minor scales (2 being a whole step and 1 being a half step)
+majorFormula = [2, 2, 1, 2, 2, 2, 1]
+minorFormula =  [2, 1, 2, 2, 1, 2, 2]
+
+# generates the notes in the scale based on key and mode
+def minorScale(key):
+    scaleNotes = []
+    scaleNotes.append(key)
+    keyIndex = notes.index(key)
+    for item in minorFormula:
+        keyIndex += item
+        if keyIndex > 11:
+            keyIndex -= 12
+        scaleNotes.append(notes[keyIndex])
+    return scaleNotes
+
+def majorScale(key):
+    scaleNotes = []
+    scaleNotes.append(key)
+    keyIndex = notes.index(key)
+    for item in majorFormula:
+        keyIndex += item
+        if keyIndex > 11:
+            keyIndex -= 12
+        scaleNotes.append(notes[keyIndex])
+    return scaleNotes
---
+++ main.py
@@ -0,0 +1,60 @@
+'''
+This is the main file of the program.
+'''
+
+import pretty_midi
+from os import system
+from random import randrange, randint
+from playsound import playsound
+import generateScale
+import chordMaker
+
+tempo = randint(80, 150)
+beatLength = 60/tempo
+print("Tempo: " + str(tempo) + " BPM")
+
+# path to the soundfont
+sfDir = "~/Documents/HackathonProject/'GeneralUser GS v1.471.sf2'"
+
+# randomly chooses key and scale
+scaleType = generateScale.scales[randrange(len(generateScale.scales))]
+keyChoice = generateScale.notes[randrange(len(generateScale.notes))]
+print("Key: " + keyChoice + " " + scaleType + "\n")
+
+# determines notes based on key and scale
+if scaleType == "Minor":
+    scaleNotes = generateScale.minorScale(keyChoice)
+elif scaleType == "Major":
+    scaleNotes = generateScale.majorScale(keyChoice)
+
+# creates the pretty_midi object
+pianoMelody = pretty_midi.PrettyMIDI()
+
+# sets the name based on MIDI standards
+piano_program = pretty_midi.instrument_name_to_program("Acoustic Grand Piano")
+piano = pretty_midi.Instrument(program=piano_program)
+
+startTime = 0
+previousNoteNumber = 0
+
+# for note_name in scaleNotes:
+#     note_number = pretty_midi.note_name_to_number(note_name + "5")
+#     if note_number < previousNoteNumber:
+#         note_number += 12
+#     note = pretty_midi.Note(velocity=127, pitch = note_number, start=startTime, end=(startTime + 0.5))
+#     piano.notes.append(note)
+#     startTime += beatLength
+#     previousNoteNumber = note_number
+
+chordProgressionRoots = []
+for x in range(1,8):
+    chordProgressionRoots.append(scaleNotes[randrange(1,7)])
+
+chordMaker.buildChord(chordProgressionRoots, scaleNotes)
+
+pianoMelody.instruments.append(piano)
+pianoMelody.write("pianoMelody.mid")
+
+# synthesizes the MIDI using the soundfont
+system("fluidsynth -ni %s pianoMelody.mid -F output.wav -r 44100 2>/dev/null" % sfDir)
+playsound("output.wav")
